9aed83edae1c57f5f6c6ea62213d8a1155b729e0
Seth Close <sethclose@gmail.com>
2014-08-06 18:01:26 -0700

    1.0

---
+++ discount_calculator.py
@@ -0,0 +1,23 @@
+def calculate_discount(item_cost, relative_discount, absolute_discount):
+    """
+    Given the cost of an item, deduct the absolute discount.
+    Then, deduct the percentage discount ("relative")
+    """
+    final_cost = item_cost
+    final_cost -= item_cost * (relative_discount / 100)
+    final_cost -= absolute_discount
+
+    return final_cost
+
+def main():
+    original = 100.00
+    relative = 10.0
+    absolute = 30.00
+    final_cost = calculate_discount(original, relative, absolute)
+    print "The original cost of ${:.2f} has been discounted.".format(original)
+    print "{:.1f}% has been taken off the original price.".format(relative)
+    print "Plus, an additional ${:.2f} discount has been made.".format(absolute)
+    print "The final, discounted price is ${:.2f}.".format(final_cost)
+
+if __name__ == "__main__":
+    main()
